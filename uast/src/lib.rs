c9a5529dad48c9091022545ddc5c3e2769b52762
ReidRussell <reidrussell217@gmail.com>
2025-12-15 23:36:46 -0500

    Minor: Expanded Statements to support While and ForLoop

--- uast/src/lib.rs
+++ uast/src/lib.rs
@@ -1,18 +1,10 @@
 use serde::{Deserialize, Serialize};
 
+// --- Core Primitives ---
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
-pub enum Statement {
-    DeclStmt(DeclStmt),
-    IfStatement(IfStatement),
-    Unknown { source: String, span: Span },
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
-pub enum ReturnType {
-    IntegerLiteral,
-    RealLiteral,
-    Identifier,
-    CharacterLiteral,
+pub struct Span {
+    pub start: usize,
+    pub end: usize,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
@@ -23,26 +15,7 @@ pub enum Literal {
     Boolean(bool),
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
-pub struct Span {
-    pub start: usize,
-    pub end: usize,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
-pub struct Block {
-    pub statements: Vec<Statement>,
-    pub span: Span,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
-pub struct IfStatement {
-    pub condition: Box<Expression>,
-    pub consequence: Box<Block>,
-    pub alternative: Option<Box<Block>>,
-    pub span: Span,
-}
-
+// --- Operators ---
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
 pub enum BinaryOperator {
     Add, Sub, Mul, Div,
@@ -73,7 +46,26 @@ pub enum BitwiseOperator {
     And, Or, Xor, LeftShift, RightShift, UnsignedRightShift
 }
 
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub enum UnaryOperator {
+    Negate,       // -x
+    Not,          // !x
+    PreIncrement, // ++x
+    PostIncrement,// x++
+    PreDecrement, // --x
+    PostDecrement,// x--
+}
 
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub enum AssignmentOperator {
+    Assign,       // =
+    AddAssign,    // +=
+    SubAssign,    // -=
+    MulAssign,    // *=
+    DivAssign,    // /=
+}
+
+// --- Expressions ---
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
 pub struct BinaryOp {
     pub left: Box<Expression>,
@@ -82,14 +74,60 @@ pub struct BinaryOp {
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub struct UnaryOp {
+    pub operator: UnaryOperator,
+    pub operand: Box<Expression>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub struct Assignment {
+    pub left: Box<Expression>,
+    pub operator: AssignmentOperator,
+    pub right: Box<Expression>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
 pub enum Expression {
     Identifier(String),
     Literal(Literal),
     BinaryOp(BinaryOp),
+    UnaryOp(UnaryOp),
+    Assignment(Assignment),
     Raw {
         source: String,
         span: Span
     },
+}
+
+// --- Statements ---
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub struct Block {
+    pub statements: Vec<Statement>,
+    pub span: Span,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub struct IfStatement {
+    pub condition: Box<Expression>,
+    pub consequence: Box<Block>,
+    pub alternative: Option<Box<Block>>,
+    pub span: Span,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub struct WhileLoop {
+    pub condition: Box<Expression>,
+    pub body: Box<Block>,
+    pub span: Span,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub struct ForLoop {
+    pub initializer: Option<Box<Statement>>,
+    pub condition: Option<Box<Expression>>,
+    pub update: Option<Box<Expression>>,
+    pub body: Box<Block>,
+    pub span: Span,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
@@ -107,11 +145,7 @@ pub struct DeclStmt {
     pub var_decl : VarDecl,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct FunctionDecl {
-    pub name: String,
-}
-
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
 pub struct ReturnStatement {
     // Need to somehow store the type
     // Temporary but could work
@@ -119,8 +153,27 @@ pub struct ReturnStatement {
     pub value: String,
 }
 
-// TODO: Implement ExpressionStatement
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub enum Statement {
+    DeclStmt(DeclStmt),
+    IfStatement(IfStatement),
+    Unknown { source: String, span: Span },
+    WhileLoop(WhileLoop), // Added this
+    ForLoop(ForLoop),     // Added this
+    ReturnStatement(ReturnStatement), // Added this
+    // TODO: Implement ExpressionStatement
+}
 
-// TODO: Implement WhileLoop
+// --- Top-Level Declarations ---
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub struct FunctionDecl {
+    pub name: String,
+}
 
-// TODO: Implement ForLoop
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub enum ReturnType {
+    IntegerLiteral,
+    RealLiteral,
+    Identifier,
+    CharacterLiteral,
+}
